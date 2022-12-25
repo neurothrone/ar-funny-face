97097e6809f7d7fc4c42ff9cc94d1edf273c7417
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-12-25 17:58:35 +0100

    finish face anchors logic

--- ARFunnyFace/ContentView.swift
+++ ARFunnyFace/ContentView.swift
@@ -5,32 +5,91 @@
 //  Created by Zaid Neurothrone on 2022-12-25.
 //
 
+import ARKit
 import RealityKit
 import SwiftUI
 
+var arView: ARView!
+
 struct ContentView : View {
+  @State var propId: Int = .zero
+  
   var body: some View {
-    ARViewContainer().edgesIgnoringSafeArea(.all)
+    ZStack(alignment: .bottom) {
+      ARViewContainer(propId: $propId)
+        .edgesIgnoringSafeArea(.all)
+      
+      HStack {
+        Spacer()
+        Button(action: decrementPropId) {
+          Image("PreviousButton")
+            .clipShape(Circle())
+        }
+        Spacer()
+        Button(action: takeSnapshot) {
+          Image("ShutterButton")
+            .clipShape(Circle())
+        }
+        Spacer()
+        Button(action: incrementPropId) {
+          Image("NextButton")
+            .clipShape(Circle())
+        }
+        Spacer()
+      }
+    }
   }
+  
+  func decrementPropId() {
+    propId = propId <= .zero ? .zero : propId - 1
+  }
+  
+  func incrementPropId() {
+    propId = propId >= 2 ? 2 : propId + 1
+  }
+  
+  func takeSnapshot() {
+    arView.snapshot(saveToHDR: false) { image in
+      let compressedImage = UIImage(
+        data: (image?.pngData())!
+      )
+      UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+    }
+  }
+  
 }
 
 struct ARViewContainer: UIViewRepresentable {
+  @Binding var propId: Int
   
   func makeUIView(context: Context) -> ARView {
-    
-    let arView = ARView(frame: .zero)
-    
-    // Load the "Box" scene from the "Experience" Reality File
-    let boxAnchor = try! Experience.loadBox()
-    
-    // Add the box anchor to the scene
-    arView.scene.anchors.append(boxAnchor)
-    
+    arView = ARView(frame: .zero)
     return arView
-    
   }
   
-  func updateUIView(_ uiView: ARView, context: Context) {}
+  func updateUIView(_ uiView: ARView, context: Context) {
+    // Remove previous anchors from other scenes
+    arView.scene.anchors.removeAll()
+    
+    let arConfig = ARFaceTrackingConfiguration()
+    uiView.session.run(arConfig, options: [.resetTracking, .removeExistingAnchors])
+    
+    switch(propId) {
+    case 0: // Eyes
+      let arAnchor = try! Experience.loadEyes()
+      uiView.scene.anchors.append(arAnchor)
+      break
+    case 1: // Glasses
+      let arAnchor = try! Experience.loadGlasses()
+      uiView.scene.anchors.append(arAnchor)
+      break
+    case 2: // Mustache
+      let arAnchor = try! Experience.loadMustache()
+      uiView.scene.anchors.append(arAnchor)
+      break
+    default: break
+    }
+  }
 }
 
 #if DEBUG
